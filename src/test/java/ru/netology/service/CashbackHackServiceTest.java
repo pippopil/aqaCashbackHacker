48cad90d72bff28ef47289c88743a6d9ff39488d
pippopil <voood234@gmail.com>
2022-02-23 15:52:18 +0300

    Added file

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,48 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CashbackHackerServiceTest {
+    public class CashbackHackServiceTest {
+
+        CashbackHackService service = new CashbackHackService();
+
+        @Test
+        public void showBelowBoundary() {
+            int purchaseAmount = 600;
+            int actualResult = service.remain(purchaseAmount);
+            int expectedResult = 400;
+            assertEquals(actualResult, expectedResult);
+
+        }
+
+        @Test
+        public void showHigherBoundary() {
+            int purchaseAmount = 1300;
+            int actualResult = service.remain(purchaseAmount);
+            int expectedResult = 700;
+            assertEquals(actualResult, expectedResult);
+
+        }
+
+        @Test
+        public void showNullBoundary() {
+            int purchaseAmount = 0;
+            int actualResult = service.remain(purchaseAmount);
+            int expectedResult = 1000;
+            assertEquals(actualResult, expectedResult);
+
+        }
+
+        @Test
+        public void showEqualBoundary() {
+            int purchaseAmount = 1000;
+            int actualResult = service.remain(purchaseAmount);
+            int expectedResult = 1000;
+            assertEquals(actualResult, expectedResult);
+
+        }
+    }
+}
